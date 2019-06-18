1ea3226c5e81b18eb6c4c20462e54da9ae9828be
Krdavis-ME <kyleRdavis-me@nau.edu>
2019-06-17 17:03:34 -0700

    updated port

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req,res) => {res.send(database.users)})
+app.get('/', (req,res) => {res.send('It is working!')})
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
@@ -38,6 +38,6 @@ app.put('/image', (req, res) => {image.handleImage(req,res,db)})
 
 app.post('/imageurl', (req, res) => {image.handleApiCall(req,res)})
 
-app.listen(3000, () => {
-    console.log('app is running!')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app is running on port ${process.env.PORT}!')
 })
